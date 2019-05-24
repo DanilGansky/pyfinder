daf11312f08e88d683820c96f5d12f27c0044cad
danildev <danya.gansky@gmail.com>
2019-05-24 19:45:44 +0300

    Placing nodes

---
+++ gui/graphics_node.py
@@ -0,0 +1,24 @@
+# Node
+
+from PyQt5.QtWidgets import QGraphicsScene, QGraphicsTextItem, QGraphicsRectItem
+from PyQt5.QtGui import QFont, QBrush, QPen, QColor
+
+class GraphicsNode(QGraphicsRectItem):
+    def __init__(self, x, y, number, scene):
+        QGraphicsRectItem.__init__(self)
+        self.setRect(x, y, 50, 50)
+        self.setBrush(QBrush(QColor(50, 50, 50)))
+        self.setPen(QPen(QColor("black")))
+        self.__x = x
+        self.__y = y
+        
+        self.number = QGraphicsTextItem(str(number))
+        self.number.setPos(x, y)
+        self.number.setZValue(10)
+        scene.addItem(self.number)
+
+    def x(self):
+        return self.__x
+
+    def y(self):
+        return self.__y
--- gui/graphics_scene.py
+++ gui/graphics_scene.py
@@ -2,18 +2,32 @@
 
 from PyQt5.QtWidgets import QGraphicsScene
 from PyQt5.QtGui import *
+from graphics_node import *
 
 class GraphicsScene(QGraphicsScene):
-    def __init__(self, parrent = None):
-        QGraphicsScene.__init__(self, parrent)
+    def __init__(self):
+        QGraphicsScene.__init__(self)
         self.setSceneRect(0, 0, 100, 100)
+        self.nodes = []
+        self.counter = 0
 
     def mousePressEvent(self, event):
-        pen = QPen(QColor(0, 0, 0))
-        brush = QBrush(QColor(50, 50, 50))
-
         x = event.scenePos().x() - 25
         y = event.scenePos().y() - 25
 
-        self.addRect(x, y, 50, 50, pen, brush)
+        self.addNode(x, y)
+       
+    def addNode(self, x, y):
+        if not self.isLocated(x, y):
+            node = GraphicsNode(x, y, self.counter, self)
+            self.nodes.append(node)
+            self.addItem(node)
+            self.counter += 1
 
+    def isLocated(self, x, y):
+        for node_in_nodes in self.nodes:
+            if (x >= node_in_nodes.x() - 125 and x <= node_in_nodes.x() + 125) and (y >= node_in_nodes.y() - 125 and y <= node_in_nodes.y() + 125):
+                return True
+
+
+        return False
